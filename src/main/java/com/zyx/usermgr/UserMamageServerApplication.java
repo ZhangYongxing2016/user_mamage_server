d3a893d0c9bb126953163f22bd4c90dd13882b0a
zyx <1769886476@qq.com>
2018-11-30 09:53:17 +0800

    初始化工程

---
+++ src/main/java/com/zyx/usermgr/UserMamageServerApplication.java
@@ -0,0 +1,12 @@
+package com.zyx.usermgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserMamageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserMamageServerApplication.class, args);
+    }
+}
